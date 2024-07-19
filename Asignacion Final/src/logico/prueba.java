b91cb03108044c7a8681028ceb2bd1e4bc25ce34
Edwin Balbuena <edwinbalbuena189@gmail.com>
2024-07-19 15:21:54 -0400

    Commit inicial

---
+++ Asignacion Final/src/logico/prueba.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package logico;
+
+/**
+ * @author edwin
+ *
+ */
+public class prueba {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
